69af41a7c3bad083c3aa0f5c3705bd0d912fb00f
Brian Multhauf <brian.multhauf@gmail.com>
2013-06-07 16:47:44 -0700

    Corrected best with worst in worst 25 hitters examples

--- spec/controllers/hitters_controller_spec.rb
+++ spec/controllers/hitters_controller_spec.rb
@@ -19,12 +19,12 @@ describe HittersController do
     end
 
     context 'assigns the 25 hitters with the worst' do
-      specify 'avg to @best_avg'
-      specify 'hr to @best_hr'
-      specify 'rbi to @best_rbi'
-      specify 'runs to @best_runs'
-      specify 'sb to @best_sb'
-      specify 'ops to @best_ops'
+      specify 'avg to @worst_avg'
+      specify 'hr to @worst_hr'
+      specify 'rbi to @worst_rbi'
+      specify 'runs to @worst_runs'
+      specify 'sb to @worst_sb'
+      specify 'ops to @worst_ops'
     end
   end
 end
